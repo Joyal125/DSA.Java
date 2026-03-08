30a16f040d2c1dd01750f4afc7509109e515e664
Joyal <joyaldaniel125@gmail.com>
2026-03-08 13:38:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check for BST/check-for-bst.java
@@ -0,0 +1,26 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public boolean isBST(Node root) {
+        return validate(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        
+    }
+    private boolean validate(Node node, long min , long max){
+        if(node==null) return true;
+        
+        if(node.data<=min || node.data >=max) return false;
+        
+        return validate(node.left,min,node.data) && validate(node.right,node.data,max);
+    }
+}
